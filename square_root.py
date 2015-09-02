0611982fa31f0c245ae9dce28250cf5c567de29e
Kathleen Rauh <krauh2010@gmail.com>
2015-09-01 21:56:31 -0400

    Added cleaner print results.

--- square_root.py
+++ square_root.py
@@ -1,11 +1,3 @@
-# If input = 0, return 0
-# otherwise,
-# Start with an input number, user_num b
-# make a rough guess of the sqrt, root_guess x
-# use that to revise the input number
-# repeat
-
-# formula = root_guess = 1/2 ( root_guess + user_num/root_guess)
 
 user_inpt = input("Enter a positive number: ")
 b = int(user_inpt)
@@ -15,15 +7,6 @@ def validate_input():
         print("This is not a positive number")
     else:
         fun_with_newton()
-#    try:
-#        value = int(user_inpt)
-#    except ValueError:
-#        print("This is not valid input.")
-
-#    if user_inpt == int(user_inpt):
-#        fun_with_newton
-#    else:
-#        print("This is not valid input.")
 
 def fun_with_newton():
     epsilon = 1
@@ -34,9 +17,8 @@ def fun_with_newton():
         new_x = (x + (b/x))/2
         epsilon = (x - new_x)
         x = new_x
-        print("After ",count, "iteration(s), the guess is", x)
-        print
+        print("After {} iteration(s), the guess is {}".format(count,x))
         count += 1
-    print("The square root of ", user_inpt, "is", x, ".")
+    print("The square root of {} user_inpt, is {}.".format(user_inpt, x))
 
 validate_input()
